c63abf7909b09377821404c5ae4d212b8958db05
xBrownieCodez <72617433+xBrownieCodezV2@users.noreply.github.com>
2024-07-12 16:43:55 +0200

    ALOT fixes and improvements

--- src/main/java/me/grax/jbytemod/JarArchive.java
+++ src/main/java/me/grax/jbytemod/JarArchive.java
@@ -1,5 +1,8 @@
 package me.grax.jbytemod;
 
+import de.xbrowniecodez.jbytemod.JByteMod;
+import lombok.Getter;
+import lombok.Setter;
 import me.grax.jbytemod.utils.ErrorDisplay;
 import me.grax.jbytemod.utils.task.LoadTask;
 import org.objectweb.asm.tree.ClassNode;
@@ -8,6 +11,8 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+@Getter
+@Setter
 public class JarArchive {
     protected Map<String, ClassNode> classes;
     protected Map<String, byte[]> output;
@@ -15,10 +20,9 @@ public class JarArchive {
     private boolean singleEntry;
 
     public JarArchive(ClassNode cn) {
-        super();
-        this.classes = new HashMap<>();
         this.singleEntry = true;
-        classes.put(cn.name, cn);
+        this.classes = new HashMap<>();
+        this.classes.put(cn.name, cn);
     }
 
     public JarArchive(JByteMod jbm, File input) {
@@ -30,36 +34,7 @@ public class JarArchive {
     }
 
     public JarArchive(Map<String, ClassNode> classes, Map<String, byte[]> output) {
-        super();
         this.classes = classes;
         this.output = output;
-    }
-
-    public Map<String, ClassNode> getClasses() {
-        return classes;
-    }
-
-    public byte[] getJarManifest() {
-        return jarManifest;
-    }
-
-    public void setJarManifest(byte[] jarManifest){
-        this.jarManifest = jarManifest;
-    }
-
-    public void setClasses(Map<String, ClassNode> classes) {
-        this.classes = classes;
-    }
-
-    public Map<String, byte[]> getOutput() {
-        return output;
-    }
-
-    public void setOutput(Map<String, byte[]> output) {
-        this.output = output;
-    }
-
-    public boolean isSingleEntry() {
-        return singleEntry;
     }
 }
